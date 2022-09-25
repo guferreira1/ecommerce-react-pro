37a9a8be69eba74dd22a9552cace9c52fe58aeb2
guferreira1 <gutoconsultor.imoveis@hotmail.com>
2022-09-25 17:33:47 -0300

    style: change Home Page div to Fragment

--- src/pages/Home/homePage.tsx
+++ src/pages/Home/homePage.tsx
@@ -4,10 +4,10 @@ import { Header } from '../../components/Header/headerComponent'
 
 export const HomePage = () => {
   return (
-    <div>
+    <>
       <Header />
 
       <Categories />
-    </div>
+    </>
   )
 }
