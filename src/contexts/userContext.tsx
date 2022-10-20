3f321f7782bc1d0b916b4c5c187a4df584e87ada
guferreira1 <gutoconsultor.imoveis@hotmail.com>
2022-10-20 17:03:37 -0300

    feat: add UserContext

---
+++ src/contexts/userContext.tsx
@@ -0,0 +1,35 @@
+import { createContext, FunctionComponent, useState } from 'react'
+import { User } from '../types/user.types'
+
+interface IUserContext {
+  currentUser: User | null
+  isAuthenticated: boolean
+  loginUser: (user: User) => void
+  logoutUser: () => void
+}
+
+export const UserContext = createContext<IUserContext>({
+  currentUser: null,
+  isAuthenticated: false,
+  loginUser: () => {},
+  logoutUser: () => {}
+})
+
+export const UserContextProvider: FunctionComponent = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState<User | null>(null)
+
+  const isAuthenticated = currentUser !== null
+
+  const loginUser = (user: User) => {
+    setCurrentUser(user)
+  }
+
+  const logoutUser = () => setCurrentUser(null)
+
+  return (
+    <UserContext.Provider
+      value={{ currentUser, isAuthenticated, loginUser, logoutUser }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
